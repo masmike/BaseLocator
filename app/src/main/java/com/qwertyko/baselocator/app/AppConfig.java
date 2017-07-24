3e02338e20f6d90c5407dd32f36139cf72a69d90
masmike <ddtika@gmail.com>
2017-07-25 06:32:11 +0700

    Initial

---
+++ app/src/main/java/com/qwertyko/baselocator/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.qwertyko.baselocator.app;
+
+/**
+ * Created by masmike on 19/07/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.43.107/apislim/logMeIn";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.3.212/surya/apiv1/register.php";
+}
